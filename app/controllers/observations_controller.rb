dfb11f0348dde41855a8e2b9dd3d10426b81114c
Nick Bell <nick.j.bell@gmail.com>
2014-10-28 20:02:15 +0100

    Add basic image gallery at observations#index
    Uses JustifiedGallery JS library to layout images. Serves images
    directly from S3.

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,5 +1,8 @@
 class ObservationsController < ApplicationController
   def index
-    @observations = Observation.order_by(:observed_at, :desc).limit(10).to_a
+    @observations = Observation.order_by(:observed_at => :desc).limit(30).to_a
+    @observations.each do |o|
+      o.image ? o.image = 'https://wingtags-syd.s3.amazonaws.com/images/s/' + o.image : ''
+    end
   end
 end
