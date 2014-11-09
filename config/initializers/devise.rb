ecda4a6ff94d8a3aaf252ff0cd3bbcef302413e9
Nick Bell <nicholas.bell@zalando.de>
2014-11-09 19:23:37 +0100

    Log warning if Devise key env var not set. fixes #1
    Specify a default key for Devise to use during initialization
    if the DEVISE_SECRET_KEY env var is not set, but log a warning.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,10 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
-  config.secret_key = 'deeb806d3f9f074f398a8395289381a1729a0cdbeb7d121bc20febb3f2319eac11373bd5cff00397f9857906877ec9587c84569536235aeea6496ba0b3d3087b'
+  config.secret_key = ENV.fetch('DEVISE_SECRET_KEY') do |var_name|
+    Rails.logger.warn "The Devise environment variable #{var_name} is not set. Using the default key value. THIS IS INSECURE."
+    'deeb806d3f9f074f398a8395289381a1729a0cdbeb7d121bc20febb3f2319eac11373bd5cff00397f9857906877ec9587c84569536235aeea6496ba0b3d3087b'
+  end
 
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
