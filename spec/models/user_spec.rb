a121d467a48a99b0a6db0cfdbfc80758faaac7c7
Nick Bell <nicholas.bell@zalando.de>
2015-05-14 16:37:27 +0200

    Create User JSON schema and test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,24 @@
 require 'rails_helper'
 
 describe User do
-  describe 'Associations' do
-    it 'should have_many Observations' do
-      pending('Dont know how to do with with NoBrainer')
+  describe '#save' do
+    it 'should save' do
+      args = FactoryGirl.attributes_for :user
+
+      user = User.new(args)
+      user.save
+
+      expect(user.persisted?).to be(true)
+    end
+
+    it 'should save the required schema' do
+      user = FactoryGirl.create(:user)
+
+      json = NoBrainer.run { |r| r.table('Spotter').get('88c0cd22-6b7e-4bfc-9914-2a9fce61d36b'); }
+      path = File.expand_path("../resources/schemas/user.schema.json", File.dirname(__FILE__))
+      schema = File.read(path)
+
+      expect(JSON::Validator.validate(schema, json)).to be(true), lambda { JSON::Validator.fully_validate(schema, json) }
     end
   end
 end
